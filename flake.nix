2908f7c8a13953014e35894b649dd3381831c51f
rej0y <zhoushengjian1@gmail.com>
2026-07-19 18:50:49 -0600

    Improve WebKit scrolling performance

--- flake.nix
+++ flake.nix
@@ -46,8 +46,6 @@
 
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (tauriNativeDeps ++ gstreamerDeps);
         GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gstreamerDeps;
-        WEBKIT_DISABLE_COMPOSITING_MODE = "1";
-
         shellHook = ''
           export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
         '';
